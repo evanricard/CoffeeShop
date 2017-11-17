df278afe91bc6acb3b02b0b545ccb45b16b3afdf
Evan Ricard <evanricard11@gmail.com>
2017-11-17 07:30:56 -0500

    First commit.

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+
+        public ActionResult Summary(UserInfo info)
+        {
+            ViewBag.Email = info.GetEmail;
+            ViewBag.Password = info.GetPassword;
+            return View();
+        }
+
+    }
+}
---
+++ CoffeeShop/Models/UserInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.Models
+{
+    public class UserInfo
+    {
+        private string email;
+        private string password;
+
+        public UserInfo()
+        {
+
+        }
+
+        public UserInfo(string email, string password)
+        {
+            this.email = email;
+            this.password = password;
+        }
+
+        public string GetEmail
+        {
+            set { email = value; }
+            get { return email; }
+        }
+
+        public string GetPassword
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
